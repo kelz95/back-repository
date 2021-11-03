2f20e9d8c2aed627aecf0760c39bfe1afbefef34
Laura Melissa Saldaña Manzano <sldmnzv@gmail.com>
2021-11-03 13:15:32 -0600

    Creación de clase Role

---
+++ tienda-api/src/main/java/com/pineapplesupermarket/tiendaapi/models/entity/Role.java
@@ -0,0 +1,70 @@
+package com.pineapplesupermarket.tiendaapi.models.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ps_role")
+public class Role implements Serializable{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id_role;
+	private String code;
+	private String description;
+	@OneToMany(mappedBy="role",fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	private List<User> users;
+
+	
+	public Role() {
+		users=new ArrayList<User>();
+	}
+
+	public Long getId() {
+		return id_role;
+	}
+
+	public void setId(Long id_role) {
+		this.id_role = id_role;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<User> getUser() {
+		return users;
+	}
+
+	public void setUser(List<User> users) {
+		this.users = users;
+	}
+	
+	public void addUser(User users) {
+		users.add(users);
+	}
+	private static final long serialVersionUID = 1L;
+
+
+}
