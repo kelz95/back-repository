cf281ad04b72deebc65aaebbb544b0667d965f85
Laura Melissa Saldaña Manzano <sldmnzv@gmail.com>
2021-11-03 13:15:47 -0600

    Creación de clase Usuario

---
+++ tienda-api/src/main/java/com/pineapplesupermarket/tiendaapi/models/entity/User.java
@@ -0,0 +1,96 @@
+package com.pineapplesupermarket.tiendaapi.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="ps_user")
+public class User implements Serializable{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id_user;
+	private String username;
+	private String password;
+	private String email;
+	private String name;
+	private String lastName;
+	@ManyToOne(fetch=FetchType.LAZY)
+	private Role role;
+	@Column(name="creation_date")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
+	
+	@PrePersist
+	public void prePersist() {
+		createAt= new Date();
+	}
+	
+	public Long getId_user() {
+		return id_user;
+	}
+
+	public void setId_user(Long id_user) {
+		this.id_user = id_user;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Role getRole() {
+		return role;
+	}
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	public Date getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+	private static final long serialVersionUID = 1L;
+	public void add(User users) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
