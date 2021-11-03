ee721bcd7450656a1ee0ccb010b862fc6e16c570
Raquel de la Rosa <abi-raq10@hotmail.com>
2021-11-02 19:07:10 -0600

    init project

---
+++ tienda-api/src/main/java/com/pineapplesupermarket/tiendaapi/TiendaApiApplication.java
@@ -0,0 +1,13 @@
+package com.pineapplesupermarket.tiendaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaApiApplication.class, args);
+	}
+
+}
