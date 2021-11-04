0feb564ceb2b4b911126581a734e1dcbdc116592
Laura Melissa Saldaña Manzano <sldmnzv@gmail.com>
2021-11-03 20:19:20 -0600

    Creación del UserService

---
+++ tienda-api/src/main/java/com/pineapplesupermarket/tiendaapi/services/UserService.java
@@ -0,0 +1,9 @@
+package com.pineapplesupermarket.tiendaapi.services;
+
+import java.util.List;
+
+import com.pineapplesupermarket.tiendaapi.models.User;
+
+public interface UserService {
+	public List<User> findAll();
+}
