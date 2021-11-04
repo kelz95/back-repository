8b247541ee0141c7f82cd0de230250639bc8f5ee
Laura Melissa Saldaña Manzano <sldmnzv@gmail.com>
2021-11-03 20:21:48 -0600

    Se agrego la anotación @Service en la clase UserServiceImpl

--- tienda-api/src/main/java/com/pineapplesupermarket/tiendaapi/services/UserServiceImpl.java
+++ tienda-api/src/main/java/com/pineapplesupermarket/tiendaapi/services/UserServiceImpl.java
@@ -3,10 +3,13 @@ package com.pineapplesupermarket.tiendaapi.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.pineapplesupermarket.tiendaapi.models.User;
 import com.pineapplesupermarket.tiendaapi.repositories.UserRepository;
 
+@Service
 public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserRepository userRepository;
