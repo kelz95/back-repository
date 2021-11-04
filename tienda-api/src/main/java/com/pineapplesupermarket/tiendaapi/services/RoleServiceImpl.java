44cfa3f7d138700e509763f01317fd387dc60fb5
Laura Melissa Saldaña Manzano <sldmnzv@gmail.com>
2021-11-03 20:21:37 -0600

    Se agrego la anotación @Service en la clase RoleServiceImpl

--- tienda-api/src/main/java/com/pineapplesupermarket/tiendaapi/services/RoleServiceImpl.java
+++ tienda-api/src/main/java/com/pineapplesupermarket/tiendaapi/services/RoleServiceImpl.java
@@ -3,11 +3,12 @@ package com.pineapplesupermarket.tiendaapi.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.pineapplesupermarket.tiendaapi.models.Role;
 import com.pineapplesupermarket.tiendaapi.repositories.RoleRepository;
-
+@Service
 public class RoleServiceImpl implements RoleService{
 	@Autowired
 	private RoleRepository roleRepository;
