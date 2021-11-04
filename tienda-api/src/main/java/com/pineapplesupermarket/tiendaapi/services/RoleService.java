6b6a84f846b97b662d7d7a39a5dc44061e3a795f
Laura Melissa Saldaña Manzano <sldmnzv@gmail.com>
2021-11-04 11:31:21 -0600

    Se agregaron al servicio role los metodos faltantes
    save
    delete
    findById

--- tienda-api/src/main/java/com/pineapplesupermarket/tiendaapi/services/RoleService.java
+++ tienda-api/src/main/java/com/pineapplesupermarket/tiendaapi/services/RoleService.java
@@ -6,4 +6,7 @@ import com.pineapplesupermarket.tiendaapi.models.Role;
 
 public interface RoleService {
 	 public List<Role> findAll();
+	 public Role save(Role role);
+	 public void delete(Long id);
+	 public Role findById(Long id);
 }
