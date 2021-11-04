fa90f6eacd394f4dc4433a8b095268199ac60231
Laura Melissa Saldaña Manzano <sldmnzv@gmail.com>
2021-11-03 21:51:40 -0600

    Se corrige Inserts en el import

--- tienda-api/src/main/resources/import.sql
+++ tienda-api/src/main/resources/import.sql
@@ -1,4 +1,4 @@
 INSERT INTO `ps_role` (`code`, `description`) VALUES ( 'ROLE_ADMIN', 'Usuario Administrador');
 INSERT INTO `ps_role` (`code`, `description`) VALUES ( 'ROLE_VIEWER', 'Usuario Regular');
 INSERT INTO `ps_user` (`id_role`,`username`, `password`, `email`, `name`,`lastname`, `creation_date`) VALUES (1, 'admin','12345','uncorreo@admin.com', 'Juan', 'Perez', '2021-11-03');
-INSERT INTO `ps_user` (`id_role`,`username`, `password`, `email`, `name`,`lastname`, `creation_date`) VALUES ( 2, 'reg','12345','otrocorreo@reg.com', 'Ana', 'Buen Día', '2021-11-03');  
+INSERT INTO `ps_user` (`id_role`,`username`, `password`, `email`, `name`,`lastname`, `creation_date`) VALUES (2, 'reg','12345','otrocorreo@reg.com', 'Ana', 'Buen Día', '2021-11-03');  
