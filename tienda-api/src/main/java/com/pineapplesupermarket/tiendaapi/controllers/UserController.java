a8e23288022be14aa1743d223c7d4b374ea902e2
Laura Melissa Saldaña Manzano <sldmnzv@gmail.com>
2021-11-04 11:22:07 -0600

    Se agrega crossorigin en el UserController

--- tienda-api/src/main/java/com/pineapplesupermarket/tiendaapi/controllers/UserController.java
+++ tienda-api/src/main/java/com/pineapplesupermarket/tiendaapi/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.pineapplesupermarket.tiendaapi.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pineapplesupermarket.tiendaapi.models.User;
 import com.pineapplesupermarket.tiendaapi.services.UserService;
 
+@CrossOrigin(origins= {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class UserController {
