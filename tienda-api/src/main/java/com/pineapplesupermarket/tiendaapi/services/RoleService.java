04bd9aa14a9255c8ac8b2d615126356355271e5c
Laura Melissa Saldaña Manzano <sldmnzv@gmail.com>
2021-11-03 20:14:50 -0600

    Creación del RoleService

---
+++ tienda-api/src/main/java/com/pineapplesupermarket/tiendaapi/services/RoleService.java
@@ -0,0 +1,9 @@
+package com.pineapplesupermarket.tiendaapi.services;
+
+import java.util.List;
+
+import com.pineapplesupermarket.tiendaapi.models.Role;
+
+public interface RoleService {
+	 public List<Role> findAll();
+}
