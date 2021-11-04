983d1a3deed58bb14c05c4f72e80b1327faec0e5
Laura Melissa Saldaña Manzano <sldmnzv@gmail.com>
2021-11-03 20:15:38 -0600

    Creación del RoleServiceImpl

---
+++ tienda-api/src/main/java/com/pineapplesupermarket/tiendaapi/services/RoleServiceImpl.java
@@ -0,0 +1,20 @@
+package com.pineapplesupermarket.tiendaapi.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pineapplesupermarket.tiendaapi.models.Role;
+import com.pineapplesupermarket.tiendaapi.repositories.RoleRepository;
+
+public class RoleServiceImpl implements RoleService{
+	@Autowired
+	private RoleRepository roleRepository;
+	@Override
+	@Transactional(readOnly=true)
+	public List<Role> findAll() {
+		return (List<Role>) roleRepository.findAll();
+	}
+
+}
