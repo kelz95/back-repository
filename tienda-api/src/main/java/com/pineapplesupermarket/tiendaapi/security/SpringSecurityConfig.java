70a735b6bfbd8713113b77305f8473d937d066cd
Raquel de la Rosa <abi-raq10@hotmail.com>
2021-11-10 15:36:56 -0600

    Cors filter prueba
    Change-Id: I3eabb507003193f762e3b230d37bfce237b47a80

--- tienda-api/src/main/java/com/pineapplesupermarket/tiendaapi/security/SpringSecurityConfig.java
+++ tienda-api/src/main/java/com/pineapplesupermarket/tiendaapi/security/SpringSecurityConfig.java
@@ -1,8 +1,12 @@
 package com.pineapplesupermarket.tiendaapi.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,6 +17,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.pineapplesupermarket.tiendaapi.servicesImpl.UserDetailsServiceImpl;
 
@@ -52,7 +60,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.cors().and().csrf().disable()
+		http.cors().configurationSource(corsConfigurationSource())
+		.and().csrf().disable()
 		.authorizeRequests()
 //		.antMatchers(HttpMethod.PUT, "/api/v1/user/restore-password").permitAll()
 //		.antMatchers(HttpMethod.PUT, "/api/v1/user/restore-password/**").permitAll()
@@ -67,5 +76,24 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("https://pineapple-front.herokuapp.com/"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+		configuration.setAllowCredentials(true);
+		configuration.setAllowedHeaders(Arrays.asList("Content-Type","Authorization","X-Requested-With"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		FilterRegistrationBean<CorsFilter> bean = 
+				new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
 
 }
