766b42889fcf3837bdefb62ce730e119510f7635
Estephany Paniagua <estephany.paniagua@utec.edu.pe>
2021-11-21 16:55:38 -0500

    productos y categorias en paginas separadas

--- tienda-front/src/modules/products/ProductsPage.tsx
+++ tienda-front/src/modules/products/ProductsPage.tsx
@@ -12,13 +12,8 @@ import Toolbar from "#root/components/Toolbar";
 import downloadFile from "#root/lib/downloadFile";
 import useDebounce from "#root/lib/hooks/useDebounce";
 import useTableOptions from "#root/lib/hooks/useTableOptions";
-import CategoriesTable from "#root/modules/categories/CategoriesTable";
 import CategoryController from "#root/modules/categories/CategoryController";
-import CreateCategoryModal from "#root/modules/categories/CreateCategoryModal";
-import UpdateCategoryModal from "#root/modules/categories/UpdateCategoryModal";
-import DeleteCategoryDialog from "#root/modules/categories/DeleteCategoryDialog";
 import { useCategoryStore } from "#root/modules/categories/useCategoryStore";
-import { Category } from "#root/modules/categories/types";
 
 import CreateProductModal from "./CreateProductModal";
 import DeleteProductDialog from "./DeleteProductDialog";
@@ -36,12 +31,7 @@ const ProductsPage = () => {
   const [isCreateProductModalOpen, setIsCreateProductModalOpen] = useState(false);
   const [isUpdateProductModalOpen, setIsUpdateProductModalOpen] = useState(false);
   const [isDeleteProductModalOpen, setIsDeleteProductModalOpen] = useState(false);
-  const [isCreateCategoryModalOpen, setIsCreateCategoryModalOpen] = useState(false);
-  const [isUpdateCategoryModalOpen, setIsUpdateCategoryModalOpen] = useState(false);
-  const [isDeleteCategoryModalOpen, setIsDeleteCategoryModalOpen] = useState(false);
 
-  const [categories, setCategories] = useState<Category[]>([]);
-  const [clickedCategory, setClickedCategory] = useState<Category | null>(null);
   const [products, setProducts] = useState<Product[]>([]);
   const [clickedProduct, setClickedProduct] = useState<Product | null>(null);
 
@@ -60,21 +50,8 @@ const ProductsPage = () => {
     const receivedCategories = res?.data || [];
 
     useCategoryStore.getState().setCategories(receivedCategories);
-    setCategories(receivedCategories);
     setIsLoading(false);
   }, []);
-
-  const handleCreateCategory = () => {
-    setIsCreateCategoryModalOpen(true);
-  };
-  const handleEditCategory = (c: Category) => {
-    setClickedCategory(c);
-    setIsUpdateCategoryModalOpen(true);
-  };
-  const handleDeleteCategory = (c: Category) => {
-    setClickedCategory(c);
-    setIsDeleteCategoryModalOpen(true);
-  };
 
   const fetchProducts = useCallback(async () => {
     setIsLoading(true);
@@ -136,11 +113,7 @@ const ProductsPage = () => {
     <>
       <NavBar />
       <Container component="main" maxWidth="lg">
-        <Typography component="h1" variant="h3" marginBottom="2rem" marginTop="1rem">
-          {t("products.inventory")}
-        </Typography>
-
-        <Typography component="h2" variant="h4" marginBottom="2rem" marginTop="1rem">
+        <Typography component="h1" variant="h4" marginBottom="2rem" marginTop="1rem">
           {t("products.products")}
         </Typography>
 
@@ -168,17 +141,6 @@ const ProductsPage = () => {
           totalRows={dataTableOptions.totalRows}
         />
 
-        <Typography component="h2" variant="h4" marginBottom="2rem" marginTop="1rem">
-          {t("products.categories")}
-        </Typography>
-
-        <Toolbar createButtonText={t("products.cCategory")} onCreate={handleCreateCategory} />
-        <CategoriesTable
-          data={categories}
-          onEdit={handleEditCategory}
-          onDelete={handleDeleteCategory}
-        />
-
         <Copyright marginTop="2rem" />
       </Container>
 
@@ -200,24 +162,6 @@ const ProductsPage = () => {
         isOpen={isDeleteProductModalOpen}
         onClose={() => setIsDeleteProductModalOpen(false)}
         onDeleteProduct={fetchProducts}
-      />
-
-      <CreateCategoryModal
-        isOpen={isCreateCategoryModalOpen}
-        onClose={() => setIsCreateCategoryModalOpen(false)}
-        onCreateCategory={fetchCategories}
-      />
-      <UpdateCategoryModal
-        data={clickedCategory}
-        isOpen={isUpdateCategoryModalOpen}
-        onClose={() => setIsUpdateCategoryModalOpen(false)}
-        onUpdateCategory={fetchCategories}
-      />
-      <DeleteCategoryDialog
-        data={clickedCategory}
-        isOpen={isDeleteCategoryModalOpen}
-        onClose={() => setIsDeleteCategoryModalOpen(false)}
-        onDeleteCategory={fetchCategories}
       />
     </>
   );
